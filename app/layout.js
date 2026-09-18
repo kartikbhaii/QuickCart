dfdf8b324e46d298e3f59f9c586210abda6266f2
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-09-18 21:24:35 +0530

    starter code and mongoDb configuration

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Outfit } from "next/font/google";
-import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
+import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
@@ -12,6 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
@@ -20,5 +22,6 @@ export default function RootLayout({ children }) {
           </AppContextProvider>
         </body>
       </html>
+      </ClerkProvider>
   );
 }
